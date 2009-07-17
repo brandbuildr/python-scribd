accbeb6a09982c70d60f3e66fa7d2650a6feafb2
arkadiusz.wahlig <arkadiusz.wahlig@f9a51dc6-5006-11de-b109-ab939168aee7>
2009-07-17 09:40:29 +0000

    Bumped version number to 1.0.0. Release will follow shortly.
    git-svn-id: http://python-scribd.googlecode.com/svn/trunk@64 f9a51dc6-5006-11de-b109-ab939168aee7

--- scribd/__init__.py
+++ scribd/__init__.py
@@ -17,7 +17,7 @@ Distributed under the new BSD License, see the
 accompanying LICENSE file for more information.
 """
 
-__version__ = '0.9.9'
+__version__ = '1.0.0'
 
 __all__ = ['NotReadyError', 'MalformedResponseError', 'ResponseError',
            'Resource', 'User', 'VirtualUser', 'Document', 'login',
